be68da535d15b3df0f0f13c4653e98ba49634c02
IdoAviramMoveo <idoa@moveo.co.il>
2024-02-05 14:40:03 +0200

    Tried the first get

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import { connectToDB } from "./db";
 const app: express.Application = express();
 const port: number = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 connectToDB();
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
