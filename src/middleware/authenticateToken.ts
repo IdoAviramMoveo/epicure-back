f678d0c47bf267b63bc5d39c92a62e29b48defe8
IdoAviramMoveo <idoa@moveo.co.il>
2024-02-19 15:24:16 +0200

    Fixed middleware

--- src/middleware/authenticateToken.ts
+++ src/middleware/authenticateToken.ts
@@ -1,11 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-interface AuthenticatedRequest extends Request {
-  user?: any;
-}
-
-export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
@@ -13,13 +9,11 @@ export const authenticateToken = (req: AuthenticatedRequest, res: Response, next
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret", (err: any, user: any) => {
+  jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret", (err: any) => {
     if (err) {
       console.log("Token verification error:", err);
       return res.sendStatus(403);
     }
-    console.log("Token is valid, user:", user);
-    req.user = user;
     next();
   });
 };
